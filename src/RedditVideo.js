e5673a154b9047b86826b04defe94fb22a70d3ba
Leon Plackal <110382152+leon-plackal@users.noreply.github.com>
2023-09-08 00:37:21 +1000

    Added check to ensure a url is provided

--- src/RedditVideo.js
+++ src/RedditVideo.js
@@ -1,21 +1,37 @@
-import {useEffect} from "react";
-import {RedditVideoPlayer} from "./index";
+import videojs from "video.js";
+import "video.js/dist/video-js.css"
 
-export default function RedditVideo({width, height, HLSurl}){
-    let styleWidth = "40rem";
-    let styleHeight = "40rem";
-    if(width){
-        styleWidth = width;
+export function RedditVideoPlayer(HLSurl, containerId) {
+    // Find the container element by its ID
+    const container = document.getElementById(containerId);
+
+    if (!container) {
+        console.error(`Container with ID "${containerId}" not found.`);
+        return;
+    } else {
+        container.innerHTML = "";
     }
-    if(height){
-        styleHeight = height;
-    }
-    useEffect(() => {
-        RedditVideoPlayer(HLSurl, "videoContainer");
-    }, []);
-    return(
-            <div style={{marginTop: "1rem"}}>
-                <div id="videoContainer" className="video-js" style={{width: `${styleWidth}`, height:`${styleHeight}` }}></div>
-            </div>
-    )
+
+    // Create a video element
+    const videoElement = document.createElement("video");
+    container.appendChild(videoElement);
+
+    // Initialize the Video.js player
+    const options = {
+        fill: true,
+        fluid: true,
+        autoplay: false,
+        controls: true,
+        preload: "metadata",
+        sources: [
+            {
+                src: HLSurl,
+                type: "application/x-mpegURL",
+            },
+        ],
+    };
+
+    const player = videojs(videoElement, options);
+
+    // Optionally, you can add event listeners or perform other actions with the player here
 }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,39 @@
-import videojs from "video.js";
-import "video.js/dist/video-js.css"
+import {useEffect} from "react";
+import {RedditVideoPlayer} from "./RedditVideo";
 
-/**
- * Initialize a Video.js player with a Reddit HLS URL and render it inside a specified container.
- * @param {string} HLSurl - URL of the HLS stream.
- * @param {string} containerId - ID of the container element where the player should be rendered.
- */
-export function RedditVideoPlayer(HLSurl, containerId) {
-    // Find the container element by its ID
-    const container = document.getElementById(containerId);
-
-    if (!container) {
-        console.error(`Container with ID "${containerId}" not found.`);
+export default function RedditVideo({width, height, HLSurl}){
+    let styleWidth = "40rem";
+    let styleHeight = "40rem";
+    if(width){
+        styleWidth = width;
+    }
+    if(height){
+        styleHeight = height;
+    }
+    if(!HLSurl){
+        console.error("Please specify a HLSurl parameter for your RedditVideo")
         return;
-    } else {
-        container.innerHTML = "";
     }
 
-    // Create a video element
-    const videoElement = document.createElement("video");
-    container.appendChild(videoElement);
+    useEffect(() => {
+        // In a non-React environment, use plain JavaScript to create and manipulate the DOM elements
+        const container = document.createElement("div");
+        container.style.marginTop = "1rem";
 
-    // Initialize the Video.js player
-    const options = {
-        fill: true,
-        fluid: true,
-        autoplay: false,
-        controls: true,
-        preload: "metadata",
-        sources: [
-            {
-                src: HLSurl,
-                type: "application/x-mpegURL",
-            },
-        ],
-    };
+        const videoContainer = document.createElement("div");
+        videoContainer.id = "videoContainer";
+        videoContainer.className = "video-js";
+        videoContainer.style.width = styleWidth;
+        videoContainer.style.height = styleHeight;
 
-    const player = videojs(videoElement, options);
+        container.appendChild(videoContainer);
 
-    // Optionally, you can add event listeners or perform other actions with the player here
+        // Call your video player initialization function here
+        RedditVideoPlayer(HLSurl, "videoContainer");
+
+        // Append the container to the document or a specific target element
+        document.body.appendChild(container);
+    }, []);
+
+    return null;
 }
